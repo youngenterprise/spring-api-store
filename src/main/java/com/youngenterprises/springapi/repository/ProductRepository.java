603ab680277dd6a0a36aa2be6cae08dec6c466e2
Nakaw <nakawyoung@gmail.com>
2022-02-16 12:10:26 -0500

    fixed security

---
+++ src/main/java/com/youngenterprises/springapi/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.youngenterprises.springapi.repository;
+
+import com.youngenterprises.springapi.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository <Product,Integer> {
+    Product findByName(String name);
+}
